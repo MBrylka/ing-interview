00bffa109132867bb97a3299e0aa7258e965f19f
Maciej Brylka <maciejbrylka94@gmail.com>
2025-06-13 16:34:10 +0100

    remove redundant fixtures

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ pytest --browser=chromium --base-url=https://www.ing.pl/
 Parametry
 
 --browser = nazwa przegladarki do uruchominia testow, aktualnie dodane firefox, chromium.
+
 --base-url = URL do testow, mozna wykorzystac do uruchamiania tych samych testow na roznych srodowiskach
 
--- conftest.py
+++ conftest.py
@@ -1,13 +1,6 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
-@pytest.fixture(scope="session")
-def browser_name(pytestconfig):
-    return pytestconfig.getoption("browser")
-
-@pytest.fixture(scope="session")
-def base_url(pytestconfig):
-    return pytestconfig.getoption("base_url")
 
 @pytest.fixture(scope="function")
 def page(browser_name, base_url):
