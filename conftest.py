4e50fc0a3d12463291cfaa488020e0de0ff50844
Maciej Bryłka <maciejbrylka94@gmail.com>
2025-06-13 20:50:36 +0200

    add stealth_sync to avoid captcha

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pathlib
 import pytest
 from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
 
 @pytest.fixture(scope="function")
 def page(browser_name, base_url):
@@ -8,6 +9,7 @@ def page(browser_name, base_url):
         browser = p[browser_name].launch(headless=True)
         context = browser.new_context(record_video_dir=pathlib.Path("videos"), )
         page = context.new_page()
+        stealth_sync(page)
         page.goto(base_url)
         yield page
         page.close()
