a186f303c6c9946638ca3cfc1d247d0f629d457f
Maciej Brylka <maciejbrylka94@gmail.com>
2025-06-13 16:56:13 +0100

    set to headless mode

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="function")
 def page(browser_name, base_url):
     with sync_playwright() as p:
-        browser = p[browser_name].launch(headless=False)
+        browser = p[browser_name].launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
         page.goto(base_url)
